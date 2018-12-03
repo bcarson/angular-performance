8bc5a02e78e53d72ac83f811f341a68e33a2e76b
Bonnie Brennan <bjcarson75@yahoo.com>
2018-12-03 13:03:56 -0600

    Add debounce to user input

--- src/app/user/user-input/user-input.component.ts
+++ src/app/user/user-input/user-input.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { debounceTime } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-user-input',
@@ -6,5 +8,16 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./user-input.component.scss'],
 })
 export class UserInputComponent {
-  @Output() search = new EventEmitter();
+  @Output() search = new EventEmitter<string>();
+  public debouncer: Subject<string> = new Subject();
+  constructor() {
+    this.debouncer.pipe(debounceTime(200)).subscribe(val => {
+      console.log('searching for: ', val);
+      this.search.emit(val);
+    });
+  }
+
+  debounce(value) {
+    this.debouncer.next(value);
+  }
 }
